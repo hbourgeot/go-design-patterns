00dce7b79138c669b9ae0518d9d456a623e6f827
Henrry Bourgeot <henrrybrgt@gmail.com>
2022-08-21 18:34:29 -0400

    Added Strategy Design Pattern

---
+++ strategy/strategy.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+// Este patrón consiste en definir una familia de algoritmos similares en estructuras separadas de tal mmanera que haya
+// una estructura base que las use y pueda intercambiar estas sin tener que modificar la forma de escribir el código
+
+type PasswordProtector struct {
+	user          string
+	passwordName  string
+	hashAlgorithm HashAlgorithm
+}
+
+type HashAlgorithm interface {
+	Hash(p *PasswordProtector)
+}
+
+func NewPasswordProtector(user string, passwordName string, hash HashAlgorithm) *PasswordProtector {
+	return &PasswordProtector{
+		user:          user,
+		passwordName:  passwordName,
+		hashAlgorithm: hash,
+	}
+}
+
+func (p *PasswordProtector) SetHashAlgortithm(hash HashAlgorithm) {
+	p.hashAlgorithm = hash
+}
+
+func (p *PasswordProtector) Hash() {
+	p.hashAlgorithm.Hash(p)
+}
+
+type SHA struct{}
+
+func (SHA) Hash(p *PasswordProtector) {
+	fmt.Printf("Hashing using SHA for %s\n", p.passwordName)
+}
+
+type MD5 struct{}
+
+func (MD5) Hash(p *PasswordProtector) {
+	fmt.Printf("Hashing using MD5 for %s\n", p.passwordName)
+}
+
+func main() {
+	sha := &SHA{}
+	md5 := &MD5{}
+
+	passwordProtector := NewPasswordProtector("henrry", "gmail password", sha)
+	passwordProtector.Hash()
+	passwordProtector.SetHashAlgortithm(md5)
+	passwordProtector.Hash()
+}
